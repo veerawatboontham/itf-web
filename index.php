33ef074d9de211a44fef85dca16f0d95db9d10c2
veerawatboontham <73679420+veerawatboontham@users.noreply.github.com>
2020-10-30 10:45:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<ch>Name: Veerawat Boontham Nickname : V </h2><br>";
 echo "Hello IT<br>";
-echo <img src="cat.jpg" />;
+echo <img src="https://images.app.goo.gl/wMRLg3VkBCRcL89D8" />;
 ?>
