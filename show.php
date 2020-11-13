524e3ff8c7ce10432473500ef7801406f10a6b0c
veerawatboontham <73679420+veerawatboontham@users.noreply.github.com>
2020-11-13 21:43:49 +0700

    Update show.php

--- show.php
+++ show.php
@@ -16,7 +16,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
+    <th width="150"> <div align="center">Action </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
