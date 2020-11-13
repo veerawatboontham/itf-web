6adad1144adc0239869fc99911c14c2db6c1e558
veerawatboontham <73679420+veerawatboontham@users.noreply.github.com>
2020-11-13 22:40:20 +0700

    Update show.php

--- show.php
+++ show.php
@@ -34,8 +34,8 @@ while($Result = mysqli_fetch_array($res))
 <?php
 mysqli_close($conn);
 ?>
-  <form action = "form.html" method = "post" id="CommentForm" >
-  <input type="เพิ่ม" id="commentBtn">
+  <form action = "insert.php" method = "post" id="CommentForm" >
+  <input type="submit" id="commentBtn">
   </form> 
 </body>
 </html>
