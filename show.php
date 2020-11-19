5bc0db00488193ea767a8d9cb188dd150a1e47f0
veerawatboontham <73679420+veerawatboontham@users.noreply.github.com>
2020-11-19 14:30:10 +0700

    Update show.php

--- show.php
+++ show.php
@@ -13,20 +13,20 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-  <tr>
+<table width="600" border="1" class="table table-striped table-dark">
+  <tr class="bg-info">
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Action </div></th>
+    <th width="150"> <div align="center">Link </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td><?php echo $Result['name'];?></div></td>
-    <td><?php echo $Result['comment'];?></td>
-    <td><?php echo $Result['link'];?></td>
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
     <td>
       <a href="edit.php?ID=<?php echo $Result['ID']?>" class="btn btn-outline-success" >EDIT</a>
       <a href="del.php?ID=<?php echo $Result['ID']?>" class="btn btn-outline-danger"onclick="return confirm('Confirm data deletion?')">DELETE</a>
@@ -36,11 +36,9 @@ while($Result = mysqli_fetch_array($res))
 }
 ?>
 </table>
+<button type="button" class="btn btn-outline-warning" onclick ="window.location.href='form.html'">ADD</button>                                                                                                       
 <?php
 mysqli_close($conn);
 ?>
-  <form action = "้form.html" method = "post" id="CommentForm" >
-  <input type="Add" id="commentBtn">
-  </form> 
 </body>
 </html>
