4f6303249cade0f38fc134d6242068aec9930bc0
veerawatboontham <73679420+veerawatboontham@users.noreply.github.com>
2020-11-19 15:08:16 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -8,9 +8,9 @@ if (mysqli_connect_errno($conn))
 }
 
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
+$name = $_POST['Name'];
+$comment = $_POST['Comment'];
+$link = $_POST['Link'];
 
 
 $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
