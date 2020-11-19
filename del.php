899b181892e542045481ae38e2f8451a4e1a4010
veerawatboontham <73679420+veerawatboontham@users.noreply.github.com>
2020-11-19 14:16:55 +0700

    Add files via upload

---
+++ del.php
@@ -0,0 +1,15 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, '63070161lab-13.mysql.database.azure.com', 'Veerawat@63070161lab-13', 'Khv30043004', 'itflab', 3306);
+$sql="DELETE FROM guestbook WHERE ID='$id'";
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+if (mysqli_query($conn, $sql)) {
+    header("Location: index.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+mysqli_close($conn);
+?>
