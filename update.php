4370fd7b444e42ca565315307d4ad298a13a2366
veerawatboontham <73679420+veerawatboontham@users.noreply.github.com>
2020-11-19 14:14:51 +0700

    Add files via upload

---
+++ update.php
@@ -0,0 +1,15 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, '63070161lab-13.mysql.database.azure.com', 'Veerawat@63070161lab-13', 'Khv30043004', 'itflab', 3306);
+$name=$_POST['Name'];
+$comment=$_POST['Comment'];
+$link=$_POST['Link'];
+$sql="UPDATE guestbook SET Name='$name',Comment='$comment',Link='$link' WHERE ID='$id'";
+if (mysqli_query($conn, $sql)) {
+    header("Location: show.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+mysqli_close($conn);
+?>
