11ee6756dc28601c09ddc3ab46bb5e74f28f796d
veerawatboontham <73679420+veerawatboontham@users.noreply.github.com>
2020-11-19 14:38:53 +0700

    Update del.php

--- del.php
+++ del.php
@@ -8,7 +8,7 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 if (mysqli_query($conn, $sql)) {
-    header("Location: show.php");
+    header("Location: index.php");
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
