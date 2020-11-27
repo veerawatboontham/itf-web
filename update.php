3e79b48694045d156d5928e4d6f12fc5040ad5e6
veerawatboontham <73679420+veerawatboontham@users.noreply.github.com>
2020-11-27 10:05:07 +0700

    Update update.php

--- update.php
+++ update.php
@@ -2,10 +2,10 @@
 $id=$_GET['ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, '63070161lab-13.mysql.database.azure.com', 'Veerawat@63070161lab-13', 'Khv30043004', 'itflab', 3306);
-$name=$_POST['Name'];
-$comment=$_POST['Comment'];
-$link=$_POST['Link'];
-$sql="UPDATE guestbook SET Name='$name',Comment='$comment',Link='$link' WHERE ID='$id'";
+$name=$_POST['Product'];
+$comment=$_POST['Price'];
+$link=$_POST['Amount'];
+$sql="UPDATE guestbook SET Product='$name',Price='$comment',Amount='$link' WHERE ID='$id'";
 if (mysqli_query($conn, $sql)) {
     header("Location: index.php");
   } else {
